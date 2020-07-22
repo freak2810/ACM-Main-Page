b28693ffbf72975a94237d1ce3be298f538a6e37
Yashwant07 <saiyashwant07@gmail.com>
2020-07-22 15:45:50 +0530

    removed about import from app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { eventData } from "./components/eventsPage/Events_DummyData"
 import Events from "./components/eventsPage/Events";
-import About from "./components/aboutPage/About";
+
 
 function App () {
   return (
