f3e129f7be61643d3c0b7355b24d1f73a1bfd5ac
nssk <nssk1999@gmail.com>
2020-08-27 17:45:38 +0530

    bilborad updated to new event

--- src/components/billboard/Billboard.jsx
+++ src/components/billboard/Billboard.jsx
@@ -1,16 +1,30 @@
 import React from "react";
 import Poster from "../../assets/Billboard/poster.png";
-import {NavLink} from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import "./Billboard.css";
 
 export default function Billboard() {
-    return <div className="billboard">
-        <a href="https://docs.google.com/forms/d/e/1FAIpQLSf-nC8f9q0uOD4cis_E9qvtD6VHG-FSoHYEjYM26CqdsUzIsQ/viewform" target="blank" className="posterLink">
-            <img src={Poster} className="poster" alt="International Workshop on Deep Learning"/>
+  return (
+    <div className="billboard">
+      <a
+        href="https://acmmvsr.typeform.com/to/VIfH3nW1"
+        target="blank"
+        className="posterLink"
+      >
+        <img
+          src={Poster}
+          className="poster"
+          alt="International Workshop on Deep Learning"
+        />
+      </a>
+      <div className="links">
+        <a href="https://acmmvsr.typeform.com/to/VIfH3nW1" target="blank">
+          Register
         </a>
-        <div className="links">
-            <a href="https://docs.google.com/forms/d/e/1FAIpQLSf-nC8f9q0uOD4cis_E9qvtD6VHG-FSoHYEjYM26CqdsUzIsQ/viewform" target="blank">Register</a>
-            <NavLink to={`/events/international-deep-learning-workshop`}>More Info</NavLink>
-        </div>
+        <NavLink to={`/events/Google-devlopers-study-jam-and-contest`}>
+          More Info
+        </NavLink>
+      </div>
     </div>
+  );
 }
